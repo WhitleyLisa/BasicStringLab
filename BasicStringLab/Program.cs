7c83721ec50099b20e720b562df6f9553f2eaa3a
Lisa Van Huijkelom <lisa.vanhuijkelom@infinicept.com>
2023-02-06 19:11:41 -0700

    Add project files.

---
+++ BasicStringLab/Program.cs
@@ -0,0 +1,31 @@
+﻿// Basic String Processing Lab
+// Prompt the user to enter a sentence.  Split the sentence into individual words and display each word on its own line.
+
+//Console.WriteLine("Please enter a sentence! :");
+//string userInput = Console.ReadLine();  
+////string[] splitList = userInput.Split(",");
+
+//for (int i = 0; i < userInput.Length; i++)
+//{
+//    Console.Write(userInput[i]);
+
+//}
+
+
+
+string splitSentence;
+string sentenceSplit = (" ");
+
+Console.WriteLine("Please enter a sentence! :");
+splitSentence = Console.ReadLine();
+sentenceSplit = splitSentence;
+
+Console.WriteLine("\n");
+
+string[] split = sentenceSplit.Split(' ');
+foreach (string item in split)
+{
+   
+    Console.WriteLine(item);
+}
+Console.ReadLine();
